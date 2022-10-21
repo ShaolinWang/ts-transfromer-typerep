48b45f3fdcb6417c1985d4713688cb6b60a603ad
wangshaolin <wangshaolin@kuaishou.com>
2022-10-21 11:32:03 +0800

    feat: add typeRep export

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { TypeRep } from './typeRep';
 
 export declare function typeRep<T>(): TypeRep;
+export { typeRep as default} from './typeRep';
 export {
   TypeKind, TypeRep, NumberRep, BooleanRep, BigIntRep, StringRep, SymbolRep, NullRep, UndefinedRep,
   VoidRep, AnyRep, UnknownRep, NeverRep, NonPrimitiveRep, UnionRep, IntersectionRep, ObjectRep
